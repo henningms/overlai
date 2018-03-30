1f1d1f882867c8d6fd955db67b597fa8ba45cbf6
Henning M. Stephansen <henningmosand@outlook.com>
2018-03-31 00:15:17 +0200

    Can enter a website to open in pip manually

--- PipTheWeb/MainPage.xaml.cs
+++ PipTheWeb/MainPage.xaml.cs
@@ -7,6 +7,7 @@ using Windows.Foundation;
 using Windows.Foundation.Collections;
 using Windows.UI;
 using Windows.UI.Core;
+using Windows.UI.Popups;
 using Windows.UI.ViewManagement;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
@@ -28,6 +29,51 @@ namespace PipTheWeb
         public MainPage()
         {
             this.InitializeComponent();
+        }
+
+        private async void OpenWebsiteButton_Click(object sender, RoutedEventArgs e)
+        {
+            var contentDialog = new ContentDialog
+            {
+                Content = new TextBox { VerticalAlignment = VerticalAlignment.Center },
+                Title = "Enter a web address",
+                PrimaryButtonText = "Ok",
+                CloseButtonText = "Cancel"
+            };
+
+            var result = await contentDialog.ShowAsync();
+
+            if (result == ContentDialogResult.Primary)
+            {
+                var url = (contentDialog.Content as TextBox).Text;
+
+                var uri = GetUriFromInput(url);
+
+                if (uri == null)
+                {
+                    await new MessageDialog("Please enter a valid address").ShowAsync();
+
+                    return;
+                }
+
+                Window.Current.Content = new WebView { Source = uri };
+
+                await ApplicationView.GetForCurrentView().TryEnterViewModeAsync(ApplicationViewMode.CompactOverlay);
+            }
+        }
+
+        private Uri GetUriFromInput(string input, bool triedAppending = false)
+        {
+            try
+            {
+                return new Uri(input);
+            }
+            catch (Exception)
+            {
+                if (triedAppending) return null;
+
+                return GetUriFromInput($"http://{input}", true);
+            }
         }
     }
 }
